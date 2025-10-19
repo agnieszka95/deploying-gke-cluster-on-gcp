30c8ea65132421dcdcfb364cb693fd88ba6163b9
Aga Bielak <agnieszka.bielak@hotmail.com>
2025-10-19 20:16:25 +0200

    Update provision.sh

--- 00_setup/provision.sh
+++ 00_setup/provision.sh
@@ -11,8 +11,10 @@ gcloud config set project "$PROJECT_ID"
 echo "Enabling container.googleapis.com"
 gcloud services enable container.googleapis.com
 
-echo "Creating cluster"
-gcloud container clusters create workshop --region=us-central1 --num-nodes=1 --machine-type="e2-micro" --release-channel="regular" --enable-autoscaling --min-nodes=1 --max-nodes=2 # Setting a small range just in case
+ZONE="us-west1-a"
+
+echo "Creating cluster 'workshop' with 1 e2-micro node in zone $ZONE"
+gcloud container clusters create workshop --zone=$ZONE --num-nodes=1 --machine-type="e2-micro" --release-channel="regular" --enable-autoscaling --min-nodes=1 --max-nodes=2
 
 echo "Getting credentials for the cluster"
 gcloud container clusters get-credentials workshop --region=us-central1
