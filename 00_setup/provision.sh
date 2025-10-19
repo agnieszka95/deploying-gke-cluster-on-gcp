41e7d15c0f355e6343cf2f5d91a5a71f4a6d6742
Aga Bielak <agnieszka.bielak@hotmail.com>
2025-10-19 21:12:09 +0200

    Update provision.sh

--- 00_setup/provision.sh
+++ 00_setup/provision.sh
@@ -13,7 +13,7 @@ gcloud services enable container.googleapis.com
 
 ZONE="us-west1-a"
 
-echo "Creating cluster 'workshop' with 1 e2-micro node in zone $ZONE"
+echo "Creating cluster 'workshop' with 1 e2-small node in zone $ZONE"
 gcloud container clusters create workshop --zone=$ZONE --num-nodes=1 --machine-type="e2-small" --release-channel="regular"
 
 echo "Getting credentials for the cluster"
