15510ae8bbeaba6f7f16d414561fb61df4c7d597
Aga Bielak <agnieszka.bielak@hotmail.com>
2025-10-19 20:12:54 +0200

    Update provision.sh

--- 00_setup/provision.sh
+++ 00_setup/provision.sh
@@ -3,22 +3,16 @@
 #set -e
 
 PROJECT_ID="MY_PROJECT_ID"
+export PROJECT_ID
 
 echo "Set Project ID"
-gcloud config set project $PROJECT_ID
+gcloud config set project "$PROJECT_ID"
 
 echo "Enabling container.googleapis.com"
 gcloud services enable container.googleapis.com
 
 echo "Creating cluster"
-gcloud container clusters create workshop --location=us-central1
+gcloud container clusters create workshop --region=us-central1 --num-nodes=1 --machine-type="e2-micro" --release-channel="regular" --enable-autoscaling --min-nodes=1 --max-nodes=2 # Setting a small range just in case
 
 echo "Getting credentials for the cluster"
-gcloud container clusters get-credentials workshop --location=us-central1
-
-echo "Create the Node Pool"
-gcloud container node-pools create workshop-nodepool --cluster workshop --zone us-central1-a --num-nodes 1 --machine-type "e2-medium" --enable-autoscaling --min-nodes=1 --max-nodes=1 --management 'auto-upgrade=true,auto-repair=true'
-
-PROJECT_NUMBER=$(gcloud config get-value project)
-PROJECT_ID=$(gcloud projects describe $PROJECT_NUMBER --format="value(projectNumber)")
-echo "Retrieved project id: $PROJECT_ID"
+gcloud container clusters get-credentials workshop --region=us-central1
